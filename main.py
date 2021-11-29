6027cc345159f86c20d981c6174be8de87717f8a
dianalorenzo <diana.lorenzo00@gmail.com>
2021-11-29 16:39:43 +0100

    added the .lower case

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 corpus_file = "resources/corpus-war-and-peace-anna-karienina.txt"
 coded_file = "resources/ciphertext.txt"
 
+#added the .lower case beacuse we do not care about the size 
+
 with open (corpus_file, 'r') as f:
-    corpus_text = f.read()
+    corpus_text = f.read().lower()
 with open (coded_file, 'r') as c:
-    coded_text = c.read()
+    coded_text = c.read().lower()
