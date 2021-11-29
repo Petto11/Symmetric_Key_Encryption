9e5f35f7010870baf553ffa978f3ecfb8bb4b56a
Petto11 <pietrovolpato20@gmail.com>
2021-11-29 22:38:39 +0100

    added the part that treats correctly the \n

--- frequency.py
+++ frequency.py
@@ -5,7 +5,9 @@ import re
 def clean(text_file):
     
     to_be_removed = "[^a-z]" #this refers to each non alphabetical character (the ^ stands for a negation)
+    second_tbr = "\n" 
 
     text_file = re.sub(to_be_removed, " ", text_file)
+    text_file = re.sub(second_tbr, " ", text_file) #removes the \n and replace them it with a blank space. 
 
     return text_file
