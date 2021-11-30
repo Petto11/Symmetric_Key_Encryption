2c7faabf9ee76b404f73e42a0b50b91e48b4f5c8
Petto11 <pietrovolpato20@gmail.com>
2021-11-30 11:54:52 +0100

    Added the frequency function

--- frequency.py
+++ frequency.py
@@ -11,3 +11,24 @@ def clean(text_file):
     text_file = re.sub(second_tbr, " ", text_file) #removes the \n and replace them it with a blank space. 
 
     return text_file
+
+
+def frequencies(text_file): 
+    
+    freq_dict = {}
+    text_file = cleaning(text_file) 
+    
+    for character in text_file:
+        
+        if character == ' ': 
+            continue
+            
+        if character not in freq_dict: 
+            freq_dict[character] = 1
+            
+        else:
+            freq_dict[character]+= 1
+            
+    freq_dict = dict(sorted(freq_dict.items(),key=lambda x:x[1],reverse=True) 
+                     
+    return freq_dict
