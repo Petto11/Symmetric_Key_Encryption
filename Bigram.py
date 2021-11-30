ef186a046504a606cc40bc515568354a95e9e60c
dianalorenzo <diana.lorenzo00@gmail.com>
2021-11-30 18:39:23 +0100

    added the function to create all the possible bigrams of a given text

--- Bigram.py
+++ Bigram.py
@@ -1,1 +1,16 @@
+import frequency
 
+def present_bigrams(text):
+
+    words = frequency.clean(text).split()
+    bigrams = [ ]
+
+    for word in words:
+        for index in range(0,len(word)-1):
+
+            bigram = word[index : index+2]
+
+            if bigram not in bigrams:
+                  bigrams+=[bigram]
+
+    return bigrams
