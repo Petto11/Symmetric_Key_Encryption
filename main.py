2594a45bbf9ec2ebe4c34cd15e0164c4f81b9911
jak414 <jak.scatto@gmail.com>
2021-11-30 18:10:51 +0100

    i tried with a random key to see if decryption function works and it seems correct

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # importare i file
+import frequency
 
 corpus_file = "resources/corpus-war-and-peace-anna-karienina.txt"
 coded_file = "resources/ciphertext.txt"
 
-#added the .lower case beacuse we do not care about the size 
+#added the .lower case beacuse we do not care about the size
 
 with open (corpus_file, 'r') as f:
     corpus_text = f.read().lower()
 with open (coded_file, 'r') as c:
     coded_text = c.read().lower()
+
+# trynna see if decryption works
+fake_key = 'qwertyuiopasdfghjklzxcvbnm'
+try1 = frequency.decryption (coded_text, fake_key)
+print(try1)
