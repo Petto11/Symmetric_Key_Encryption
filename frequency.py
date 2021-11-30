ad64a9c2208f4e0effcb00a52b509b4031bc1d84
ErChico6 <nicolo.vian@student.h-farm.com>
2021-11-30 17:17:54 +0100

    corrected the order

--- frequency.py
+++ frequency.py
@@ -4,11 +4,11 @@ import re
 
 def clean(text_file):
     
-    to_be_removed = "[^a-z]" #this refers to each non alphabetical character (the ^ stands for a negation)
-    second_tbr = "\n" 
+    second_to_be_removed = "[^a-z]" #this refers to each non alphabetical character (the ^ stands for a negation)
+    first_tbr = "\n" 
 
-    text_file = re.sub(to_be_removed, " ", text_file)
-    text_file = re.sub(second_tbr, " ", text_file) #removes the \n and replace them it with a blank space. 
+    text_file = re.sub(second_to_be_removed, " ", text_file)
+    text_file = re.sub(first_tbr, " ", text_file) #removes the \n and replace them it with a blank space. 
 
     return text_file
 
