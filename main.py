3aa2f6bec46b714bd992dbd8ce2195ce41103e38
jak414 <jak.scatto@gmail.com>
2022-01-25 19:25:10 +0100

    PEP8 compliance

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 import frequency
 import bigram
 import argparse
+import type_control
 
-parser = argparse.ArgumentParser(description='Decrypts the text \
+parser = argparse.ArgumentParser(description='Decrypts the text\
                                  given as input')
 parser.add_argument("-text", type=argparse.FileType('r'),
                     help="Encrypted text to be decrypted")
@@ -12,51 +13,49 @@ parser.add_argument("--length", type=int,
                     help=" N° of characters in the plaintext to be displayed")
 parser.add_argument("--brute", type=str,
                     help="USe or not brute force")
+parser.add_argument("--digest", type=argparse.FileType('r'),
+                    help="If you have the digest of the original\
+                    message you can insert it here")
+
 args = parser.parse_args()
 length = args.length
 corpus_file = "resources/corpus-war-and-peace-anna-karienina.txt"
 
 cipher = str(args.text)
+digest = str(args.digest)
+
 
 # added the .lower case beacuse we do not care about the size
-
 with open(corpus_file, 'r') as f:
     corpus_text = f.read().lower()
 
-if type_control.file_type(cipher):
-    cipher_text = args.text.read()
+if type_control.file_type(cipher) is True:
 
-    first_key, first_plaintext =
-    frequency.frequency_approach(corpus_text,
-                                  cipher_text)
+    cipher_text = args.text.read()
+    first_key, first_plaintext = frequency.frequency_approach(corpus_text,
+                                                              cipher_text)
+
     if len(frequency.clean(cipher_text)) < 5000:
-        print("Since the analysis made on the \
-              encrypted text is based on statystical \
-              properties of a language, the program \
-              will work only if the text to decrypt \
-              is at least 5000 characters long \
-              (numbers and punctuation excluded)")
+        print("Since the analysis made on the encrypted text is based on \
+        statystical properties of a language, the program will work only \
+        if the text to decrypt is at least 5000 characters long (numbers \
+        and punctuation excluded)")
+
     elif args.brute == 'Y':
-        print(bigram.brute_force(cipher_text, first_key,
-                                 bigram.bigram_freq(corpus_text),
-                                 args.length, digest_text))
+
+        if type_control.file_type(digest) is True:
+            digest_text = args.digest.read()
+            print(bigram.brute_force(cipher_text, first_key,
+                                     bigram.bigram_freq(corpus_text),
+                                     args.length, digest_text))
+
         else:
             print(bigram.brute_force(cipher_text, first_key,
                                      bigram.bigram_freq(corpus_text),
                                      args.length, " "))
+
     elif args.brute != 'Y':
-    first_key,first_plaintext=frequency.frequency_approach(corpus_text,cipher_text)
-    if len(frequency.clean(cipher_text))<5000:
-        print("Since the analysis made on the encrypted text is based on statystical properties of a language, the program will work only if the text to decrypt is at least 5000 characters long (numbers and punctuation excluded)")
-    elif args.brute == 'Y'
-        print(bigram.brute_force(cipher_text,first_key, bigram.bigram_freq(corpus_text), args.length,digest_text))
-        else:
-            print(bigram.brute_force(cipher_text,first_key, bigram.bigram_freq(corpus_text), args.length," "))
-    elif args.brute!='Y':
         print(first_plaintext)
-if args.brute == 'Y':
-    print(bigram.brute_force(cipher_text,
-                             first_key,  bigram.bigram_freq(corpus_text),
-                             args.length, digest_text))
+
 else:
     print('You must insert a .txt file, other formats are not accepted')
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,44 +2,43 @@ import unittest
 import sys
 import os
 
-#sys.path.append("..")
+# sys.path.append("..")
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-import frequency
-import bigram
+import frequency    # noqa: E402 (impossible to import at top of file)
+import bigram       # noqa: E402
 
 # add parent folder to path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-
 class TestInput(unittest.TestCase):
-    
+
     # smoke test: valid inputs
     def test_correct_values(self):
 
-        self.assertEqual(frequency.clean("\n tieni solo questo 414!\n"), "  tieni solo questo      ")
-        self.assertEqual(frequency.frequencies("Nel mezzo del cammin di nostra vita")['a'], 3)
-        self.assertEqual(bigram.present_bigrams("hello"), ["he", "el", "ll", "lo"])
-        self.assertEqual(bigram.swap("palo","p","l"), "lapo")
+        self.assertEqual(frequency.clean("\n tieni solo questo 414!\n"),
+                         "  tieni solo questo      ")
+        self.assertEqual(frequency.frequencies(
+                "Nel mezzo del cammin di nostra vita")['a'], 3)
+        self.assertEqual(bigram.present_bigrams("hello"),
+                         ["he", "el", "ll", "lo"])
+        self.assertEqual(bigram.swap("palo", "p", "l"), "lapo")
 
     # invalid inputs
     def test_wrong_values(self):
 
-        self.assertEqual(frequency.keys("Exactlytwentysixcharacters","too_few_characters"), None)
-
+        self.assertEqual(frequency.keys("Exactlytwentysixcharacters",
+                         "too_few_characters"), None)
 
     # corner case: empty string
     def test_empty_string(self):
 
         self.assertEqual(frequency.clean(""), None)
 
+
 if __name__ == '__main__':
 
-    # basic test
-    #unittest.main()
-
-    # with more details
     unittest.main(verbosity=2)
